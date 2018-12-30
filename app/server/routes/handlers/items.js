b325a16cf0828e4a26568556dd3c677b2ef890d0
Sergio Miranda <sjamr10@gmail.com>
2018-12-30 18:05:47 -0300

    Format results according to test indications

--- app/server/routes/handlers/items.js
+++ app/server/routes/handlers/items.js
@@ -8,10 +8,34 @@ const render = (req, res) => {
 
   loadSSRData(search)
     .then((response) => {
+      const { results } = response[0].data;
+
+      const items = results.slice(0, 4).map((item) => ({
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: item.price,
+          decimals: 0,
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping,
+      }));
+
       const ssrData = {
         search,
-        items: response[0].data.results,
+        results: {
+          author: {
+            name: 'Sergio',
+            lastname: 'Miranda',
+          },
+          categories: [],
+          items,
+        },
       };
+
+      console.log(ssrData);
 
       res.locals.REACT_STATE = {
         ...res.locals.REACT_STATE,
