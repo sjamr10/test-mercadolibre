51ccd4877da4672070b24f9afd71108a9a251c00
Sergio Miranda <sjamr10@gmail.com>
2018-12-31 19:26:26 -0300

    Add product info

--- app/client/pages/Product/components/ProductDetails/index.jsx
+++ app/client/pages/Product/components/ProductDetails/index.jsx
@@ -8,19 +8,57 @@ if (!__SSR__) {
 const ProductDetails = (props) => {
   const { product } = props;
 
-  return (
-    <div className="product-details">
-      <div className="product row">
-        <div className="picture-col col-12 col-sm-auto">
-          <div className="picture">
-            {product ? <img src={product.picture} alt="Foto del producto" /> : ''}
+  if (product) {
+    return (
+      <div className="product-details">
+        <div className="product row">
+          <div className="picture-col col-8">
+            <div className="picture">
+              <img src={product.picture} alt="Foto del producto" />
+            </div>
+          </div>
+          <div className="info col-4">
+            <div className="row">
+              <div className="condition col">
+                {product.condition === 'new' ? 'Nuevo' : 'Usado'} - {product.sold_quantity} vendidos
+              </div>
+            </div>
+            <div className="row">
+              <div className="title col">
+                {product.title}
+              </div>
+            </div>
+            <div className="row">
+              <div className="price col">
+                $ {product.price.amount}
+              </div>
+            </div>
+            <div className="row">
+              <div className="button col">
+                Button
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="description row">
+          <div className="description-col col-12">
+            <div className="title row">
+              <div className="col">
+                Drescripción del producto
+              </div>
+            </div>
+            <div className="row">
+              <div className="text col-7">
+                {product.description}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="description row">
-        <div className="picture-col col-12 col-sm-auto" />
-      </div>
-    </div>
+    );
+  }
+  return (
+    <div className="product-details" />
   );
 };
 
