5d4f32dc1011dc52151fabe9c3266253d46bd0df
Sergio Miranda <sjamr10@gmail.com>
2018-12-31 14:27:41 -0300

    Remove unused request code

--- app/server/routes/main.js
+++ app/server/routes/main.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const request = require('request');
 
 const items = require('./handlers/items');
 
-
-const {
-  API,
-} = TEST_MERCADOLIBRE;
 
 const router = express.Router();
 
@@ -18,23 +13,6 @@ router.get('/items', items.default.render);
 
 router.get('/:id', (req, res) => {
   res.render('items');
-});
-
-router.use('/api', (req, res) => {
-  console.log(`${req.url}`);
-  request({
-    url: `${req.url}`,
-    method: req.method,
-    json: req.body,
-    timeout: API.TIMEOUT,
-    forever: true,
-    headers: {
-    },
-  })
-    .on('error', (err) => {
-      console.log(err);
-    })
-    .pipe(res);
 });
 
 module.exports = router;
