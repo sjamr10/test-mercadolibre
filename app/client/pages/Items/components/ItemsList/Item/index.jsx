2575a591869782ad813ce07aa3a26b66a830bd78
Sergio Miranda <sjamr10@gmail.com>
2018-12-30 22:10:06 -0300

    Add picture and state_name

--- app/client/pages/Items/components/ItemsList/Item/index.jsx
+++ app/client/pages/Items/components/ItemsList/Item/index.jsx
@@ -9,8 +9,16 @@ if (!__SSR__) {
 const Item = (props) =>
   (
     <div className="item row justify-content-sm-center">
-      <div className="col-12 text-center">
+      <div className="col-3 text-center">
+        <div className="picture">
+          <img src={props.item.picture} alt="Logo de Mercadolibre" />
+        </div>
+      </div>
+      <div className="col-6 text-center">
         {props.item.title}
+      </div>
+      <div className="col-3 text-center">
+        {props.item.state_name}
       </div>
     </div>
   );
