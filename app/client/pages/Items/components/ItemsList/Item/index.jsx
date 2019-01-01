116da0c9546ff6ab0c6a956701c81caa5459701d
Sergio Miranda <sjamr10@gmail.com>
2019-01-01 19:06:56 -0300

    Add onClick to Item

--- app/client/pages/Items/components/ItemsList/Item/index.jsx
+++ app/client/pages/Items/components/ItemsList/Item/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Formatter from 'app/lib/formatter';
 
 
@@ -7,38 +7,57 @@ if (!__SSR__) {
 }
 
 
-const Item = (props) =>
-  (
-    <div className="rcc-item row">
-      <div className="picture-col col-12 col-sm-auto">
-        <div className="picture">
-          <img src={props.item.picture} alt="Foto del producto" />
-        </div>
-      </div>
-      <div className="main col-12 col-sm-7 col-md-5">
-        <div className="row">
-          <div className="price col-sm-auto">
-            $ {Formatter.thousands(props.item.price.amount)}
+class Item extends Component {
+  constructor(props) {
+    super(props);
+
+    this.form = React.createRef();
+  }
+
+  goToProductPage = () => {
+    this.form.current.submit();
+  }
+
+  render() {
+    return (
+      <div className="rcc-item row" onClick={this.goToProductPage}>
+        <form
+          ref={this.form}
+          className="d-none"
+          action={`/items/${this.props.item.id}`}
+          method="get"
+        />
+        <div className="picture-col col-12 col-sm-auto">
+          <div className="picture">
+            <img src={this.props.item.picture} alt="Foto del producto" />
           </div>
-          <div className="free-shipping-col col-sm-auto">
-            <div className="free-shipping">
-              {props.item.free_shipping ? <img src="/images/ic_shipping2.png" alt="Logo de envío gratuito" /> : ''}
+        </div>
+        <div className="main col-12 col-sm-7 col-md-5">
+          <div className="row">
+            <div className="price col-sm-auto">
+            $ {Formatter.thousands(this.props.item.price.amount)}
+            </div>
+            <div className="free-shipping-col col-sm-auto">
+              <div className="free-shipping">
+                {this.props.item.free_shipping ? <img src="/images/ic_shipping2.png" alt="Logo de envío gratuito" /> : ''}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="title col-12 col-sm-10">
+              {this.props.item.title}
+            </div>
+            <div className="condition col-12 col-sm-2">
+              {this.props.item.condition === 'new' ? 'Nuevo' : 'Usado'}
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="title col-12 col-sm-10">
-            {props.item.title}
-          </div>
-          <div className="condition col-12 col-sm-2">
-            {props.item.condition === 'new' ? 'Nuevo' : 'Usado'}
-          </div>
+        <div className="state-name col-12 col-md-3 text-center">
+          {this.props.item.state_name}
         </div>
       </div>
-      <div className="state-name col-12 col-md-3 text-center">
-        {props.item.state_name}
-      </div>
-    </div>
-  );
+    );
+  }
+}
 
 export default Item;
