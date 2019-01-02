8dfa176b15917d38fddf8c00a59ff5b7af166469
Sergio Miranda <sjamr10@gmail.com>
2019-01-02 16:31:59 -0300

    Add onClick to logo

--- app/client/components/Header/index.jsx
+++ app/client/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import SearchBox from 'app/client/components/SearchBox';
 
@@ -8,15 +8,35 @@ if (!__SSR__) {
 }
 
 
-const Header = () => (
-  <div className="rcc-header row justify-content-sm-center">
-    <div className="logo col-12 col-sm-1">
-      <img src="/images/Logo_ML2x.png" alt="Logo de Mercadolibre" />
-    </div>
-    <div className="search-box col-12 col-sm-8 col-md-6">
-      <SearchBox />
-    </div>
-  </div>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.form = React.createRef();
+  }
+
+  goToHomePage = () => {
+    this.form.current.submit();
+  }
+
+  render() {
+    return (
+      <div className="rcc-header row justify-content-sm-center">
+        <form
+          ref={this.form}
+          className="d-none"
+          action="/"
+          method="get"
+        />
+        <div className="logo col-12 col-sm-1" onClick={this.goToHomePage}>
+          <img src="/images/Logo_ML2x.png" alt="Logo de Mercadolibre" />
+        </div>
+        <div className="search-box col-12 col-sm-8 col-md-6">
+          <SearchBox />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Header;
